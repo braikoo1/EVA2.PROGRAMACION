8838ed76985aace5a5fcdcded425628c6abaf5c7
braikoo1 <brayan.bnzf@gmail.com>
2025-05-26 14:39:10 -0400

    commit final

--- script2.js
+++ script2.js
@@ -1,27 +1,36 @@
 //validar nombre, debe contener solo letras y no ser vacio
 let personas=[]
-function validarNombre(){
+function validar(){
     let eNombre = document.getElementById("nombre")
-    let vNombre= eNombre.value
-    let eErrorNombre= document.getElementById("errorNombre")
-
-
+    let vNombre = eNombre.value
+    let eErrorNombre = document.getElementById("errorNombre")
+    let eEdad = document.getElementById("edad")
+    let vEdad = eEdad.value
+    let eErrorEdad = document.getElementById("errorEdad")
+    if (vNombre == ""){
+        console.log("El nombre no puede estar vacio") 
+        alert("El nombre no puede estar vacio")  
+    }
+    if (vEdad <18){
+        console.log("Debes ser mayor de 18 años")
+        alert("Debes ser mayor de 18 años")
+    }
+    if (vEdad <100){
+        console.log("Debes poner una edad valida (menor que 100)")
+        alert("Debes poner una edad valida (menor que 100)")
+    }
 }
+
 
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
-
-function validarEdad(){
-    let eEdad = document.getElementById("edad")
-    let vEdad= eEdad.value
-    let eErrorEdad= document.getElementById("errorEdad")
-}
+    
 
 function cargarDatos(){
 
 }
-
 function actualizarDatos(){
 
-    
+
 }
+
 
